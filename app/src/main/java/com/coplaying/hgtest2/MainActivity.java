1a19a4cad2c058e7a2cafab21d0fb9ec1bd4af05
hyungang <hyungang.yang@huiseoul.com>
2017-03-29 17:42:52 +0900

    MainActivity에 RecycleView 구현

--- app/src/main/java/com/coplaying/hgtest2/MainActivity.java
+++ app/src/main/java/com/coplaying/hgtest2/MainActivity.java
@@ -4,13 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView memoList;
+    private MemoAdapter memoAdapter;
+    private RecyclerView.LayoutManager memoLayoutManager;
+    String[] dataSet;
+    int dataSetindex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +29,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        dataSet = new String[10];
+        dataSet[0] = "Initial contents1";
+        dataSet[1] = "Initial contents2";
+        dataSetindex = 1;
+
+        memoList = (RecyclerView) findViewById(R.id.memo_list);
+        memoLayoutManager = new LinearLayoutManager(this);
+        memoList.setLayoutManager(memoLayoutManager);
+        memoAdapter = new MemoAdapter(dataSet);
+        memoList.setAdapter(memoAdapter);
 
         FloatingActionButton add = (FloatingActionButton) findViewById(R.id.add);
         add.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +60,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if(resultCode == 1){
             String content = data.getStringExtra("memo_content");
+            dataSetindex++;
+            dataSet[dataSetindex] = content;
+            memoAdapter.swapData(dataSet);
+            memoAdapter.notifyDataSetChanged();
             Toast.makeText(MainActivity.this, content, Toast.LENGTH_LONG).show();
         }
     }
@@ -62,5 +88,44 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class MemoAdapter extends RecyclerView.Adapter<MemoAdapter.ViewHolder> {
+        private String[] memoSet;
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView memoText;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                memoText = (TextView) itemView.findViewById(R.id.memo_text);
+            }
+        }
+
+        public MemoAdapter(String[] dataSet){
+            memoSet = dataSet;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.memo_text_view, parent, false);
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.memoText.setText(memoSet[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return memoSet.length;
+        }
+
+        public void swapData(String[] dataSet) {
+            memoSet = dataSet;
+        }
     }
 }
